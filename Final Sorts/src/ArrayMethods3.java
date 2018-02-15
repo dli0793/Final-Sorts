f248ca8e4d866efc8431cee9f066ffa6bce212f5
BT_1N3_31 <BT_1N3_31@BT_1N3_31.bktech.local>
2018-02-15 09:22:33 -0500

    :)

--- Final Sorts/src/ArrayMethods3.java
+++ Final Sorts/src/ArrayMethods3.java
@@ -5,7 +5,35 @@ public class ArrayMethods3
 
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
+		//Testing Arrays
+		String[] list1 = {"microsoft", "apple", "cucumber", "zorro", "tangerine"};
+		String[] list2 = {"banana", "cherry", "mahogany", "oreos", "pinata"};
+		int[] list3 = {3, 4, 2, 7, 12, 22, 0, 1};
+				
+		//Merge Test
+		long start = System.nanoTime();
+		mergeSort(list1);
+		long end = System.nanoTime();
+		long time = end-start;
+		System.out.println("MergeSort test took: " + time + "nanoseconds.");
+		System.out.println(java.util.Arrays.toString(mergeSort(list1)));
+				
+		//Partition Test
+		start = System.nanoTime();
+		int pivotFinal = partition(list3, 0, 7);
+		end = System.nanoTime();
+		time = end-start;
+		System.out.println("Partition test took: " + time + "nanoseconds.");
+		System.out.println("Final pivot position: " + pivotFinal);
+		System.out.println(java.util.Arrays.toString(list3));
+		
+		//QuickSort Test
+		start = System.nanoTime();
+		quickSort(list3, 0, 7);
+		end = System.nanoTime();
+		time = end-start;
+		System.out.println("QuickSort test took: " + time + "nanoseconds.");
+		System.out.println(java.util.Arrays.toString(list3));
 
 	}
 	
@@ -18,15 +46,69 @@ public class ArrayMethods3
 		
 		int center = list1.length/2;
 		String[] half1 = Arrays.copyOfRange(list1, 0, center);
-		String[] half2 = Arrays.copyOfRange(list1, center+1, list1.length);
+		String[] half2 = Arrays.copyOfRange(list1, center+1, list1.length-1);
 		
 		return merge(mergeSort(half1), mergeSort(half2));
 	}
 
-	private static String[] merge(String[] mergeSort, String[] mergeSort2) {
+	public static String[] merge(String[] list1, String[] list2)
+	{
+		String[] x;
+		String[] z;
 		
-		// TODO Auto-generated method stub
-		return null;
+		if(list1.length<list2.length)
+		{
+			x = list1;
+			z = list2;
+		}
+		else
+		{
+			x = list2;
+			z = list1;
+		}
+		
+		String[] y = new String[z.length+x.length];
+		int r = 0;
+		for(int i = 0;i<x.length;i++)
+		{
+			if(list1[i].compareTo(list2[i])<0)
+			{
+				y[r] = list1[i];
+				y[r+1] = list2[i];
+				r+=2;
+			}
+			
+			else
+			{
+				y[r] = list2[i];
+				y[r+1] = list1[i];
+				r+=2;
+			}
+		}
+		
+		for(int i = 0;i<z.length;i++)
+		{
+			y[r] = z[i];
+		}
+		
+		String tempStr;
+		int counter = 1;
+		while(counter>0)
+		{
+			counter = 0;
+			for(int i=0;i<y.length-1;i++)
+			{	
+				if(y[i].compareTo(y[i+1])>0)
+				{
+					tempStr = y[i];
+					y[i] = y[i+1];
+					y[i+1] = tempStr;
+					counter = 1;
+				}
+			}
+		}
+		
+		return y;
 	}
 	
 	public static int partition(int[] list1, int front, int back)
@@ -55,6 +137,16 @@ public class ArrayMethods3
 	    	}
 	    return index;
 	
+	}
+	
+	public static void quickSort(int[] list1, int front, int back)
+	{
+		if(back>front)
+		{
+			int pivotPos = partition(list1, front, back);
+			quickSort(list1, front, pivotPos-1);
+			quickSort(list1, pivotPos+1, back);
+		}
 	}
 
 }
